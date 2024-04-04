ae64274f1ec64160474c7dcc5d4c485571e073fe
Mayara Medino <medino.dev@gmail.com>
2024-04-04 22:48:14 +0000

    MAIN

---
+++ Java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+  public static void main(String[] args) {
+
+  Primos primo = new Primos();
+  primo.chamarPrimos();
+
+  Somatorio somatorios = new Somatorio();
+  somatorios.chamarSomatorio();
+
+  Contagem contagem = new Contagem();
+  contagem.chamarContagem();
+
+  Fibonacci fibonacci = new Fibonacci();
+  fibonacci.chamarFibonacci();
+
+  Mdc mdc = new Mdc();
+  mdc.chamarMdc();
+
+  QuickSort quicksort = new QuickSort();
+  quicksort.chamarQuicksort();
+    
+  }
+}
